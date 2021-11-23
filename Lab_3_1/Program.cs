b7464fc77e0ca8fa16d85bacbca1051925c86e99
IgoninMihail <92269370+IgoninMihail@users.noreply.github.com>
2021-11-23 17:12:22 +0200

    Добавьте файлы проекта.

---
+++ Lab_3_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите число N:");
+                int N = int.Parse(Console.ReadLine());
+                for (int i = 10; i <= N; i++)
+                {
+                    int S = 0;
+                    int m = 0;
+                    int k = i;
+                    while (k != 0)
+                    {
+                        m = k % 10;
+                        S += k % 10;
+                        k /= 10;
+                    }
+                    Console.WriteLine(i + " " + m + " " + S);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
